23b45f501a07038963e07429ec78d7db10805dc6
Fay Ash <yaoshiu@qq.com>
2025-02-17 02:46:09 +0800

    nixvim: add prettier to typescript

--- editors/nixvim/languages/typescript/default.nix
+++ editors/nixvim/languages/typescript/default.nix
@@ -1,10 +1,35 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.nixvim = {
-    plugins.lsp.servers = {
-      ts_ls = {
-        enable = true;
+    plugins = {
+      lsp.servers = {
+        ts_ls = {
+          enable = true;
+        };
       };
+
+      conform-nvim.settings.formatters_by_ft =
+        let
+          supported = [
+            "javascript"
+            "typescript"
+            "javascriptreact"
+            "typescriptreact"
+            "css"
+            "json"
+            "vue"
+          ];
+        in
+        builtins.listToAttrs (
+          map (supported: {
+            name = supported;
+            value = [ "prettier" ];
+          }) supported
+        );
     };
+
+    extraPackages = with pkgs; [
+      nodePackages.prettier
+    ];
   };
 }
