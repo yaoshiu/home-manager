7c56aecdb8abfc8b1e5fdbc9d7d5efb7e87ce6d6
Fay Ash <yaoshiu@qq.com>
2025-02-21 17:42:22 +0800

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -2,9 +2,6 @@
   description = "Home Manager configuration of yaoshiu";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
