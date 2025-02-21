7a1faf4241533c035c385f5acef9316caec6e0cb
Fay Ash <yaoshiu@qq.com>
2025-02-21 17:42:32 +0800

    nixvim: add keymaps for trouble and noice

--- editors/nixvim/editor/trouble/default.nix
+++ editors/nixvim/editor/trouble/default.nix
@@ -3,7 +3,21 @@
   programs.nixvim = {
     plugins.trouble = {
       enable = true;
-      lazyLoad.settings.cmd = "Trouble";
+      lazyLoad.settings = {
+        cmd = "Trouble";
+        keys = [
+          {
+            __unkeyed-1 = "<leader>xx";
+            __unkeyed-2 = "<cmd>Trouble diagnostics toggle<cr>";
+            desc = "Diagnostics";
+          }
+          {
+            __unkeyed-1 = "<leader>xX";
+            __unkeyed-2 = "<cmd>Trouble diagnostics toggle filter.buf=0<cr>";
+            desc = "Buffer Diagnostics";
+          }
+        ];
+      };
     };
   };
 }
--- editors/nixvim/ui/noice/default.nix
+++ editors/nixvim/ui/noice/default.nix
@@ -3,7 +3,36 @@
   programs.nixvim = {
     plugins.noice = {
       enable = true;
-      lazyLoad.settings.event = "DeferredUIEnter";
+      lazyLoad.settings = {
+        event = "DeferredUIEnter";
+        keys = [
+          {
+            __unkeyed-1 = "<leader>sn";
+            __unkeyed-2 = "";
+            desc = "+noice";
+          }
+          {
+            __unkeyed-1 = "<leader>snl";
+            __unkeyed-2 = "<cmd>Noice last<cr>";
+            desc = "Noice Last Message";
+          }
+          {
+            __unkeyed-1 = "<leader>snh";
+            __unkeyed-2 = "<cmd>Noice history<cr>";
+            desc = "Noice History";
+          }
+          {
+            __unkeyed-1 = "<leader>sna";
+            __unkeyed-2 = "<cmd>Noice all<cr>";
+            desc = "Noice All";
+          }
+          {
+            __unkeyed-1 = "<leader>snd";
+            __unkeyed-2 = "<cmd>Noice dismiss<cr>";
+            desc = "Dismiss All";
+          }
+        ];
+      };
 
       settings = {
         lsp.override = {
