718555898f3318c2e002b491dd809979f555473b
Fay Ash <yaoshiu@qq.com>
2025-06-07 23:37:05 +0800

    nixvim: bufferline configuration

--- editors/nixvim/ui/bufferline/default.nix
+++ editors/nixvim/ui/bufferline/default.nix
@@ -3,6 +3,26 @@
   programs.nixvim = {
     plugins.bufferline = {
       enable = true;
+      lazyLoad.settings.event = "DeferredUIEnter";
+      settings = {
+        options = {
+          close_command.__raw = "function(n) Snacks.bufdelete(n) end";
+          right_mouse_command.__raw = "function(n) Snacks.bufdelete(n) end";
+          diagnostics = "nvim_lsp";
+          always_show_bufferline = false;
+          offsets = [
+            {
+              filetype = "neo-tree";
+              text = "Neo-tree";
+              highlight = "Directory";
+              text_align = "left";
+            }
+            {
+              filetype = "snacks_layout_box";
+            }
+          ];
+        };
+      };
     };
   };
 }
