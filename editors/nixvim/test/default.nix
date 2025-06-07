b455b130085c4de98a9734663f1b38377a0221a7
Fay Ash <yaoshiu@qq.com>
2025-06-07 23:36:46 +0800

    nixvim: neotest configuration

--- editors/nixvim/test/default.nix
+++ editors/nixvim/test/default.nix
@@ -1,4 +1,7 @@
-{ ... }:
+{ config, ... }:
+let
+  helpers = config.lib.nixvim;
+in
 {
   programs.nixvim = {
     plugins = {
@@ -8,8 +11,56 @@
           status = {
             virtual_text = true;
           };
+          output = {
+            open_on_run = true;
+          };
         };
         lazyLoad.settings = {
+          after.__raw = ''
+            function()
+              local opts = ${helpers.lua.toLuaObject config.programs.nixvim.plugins.neotest.settings}
+              vim.diagnostic.config({
+                virtual_text = {
+                  format = function(diagnostic)
+                    -- Replace newline and tab characters with space for more compact diagnostics
+                    local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+                    return message
+                  end,
+                },
+              }, neotest_ns)
+
+              opts.consumers = opts.consumers or {}
+              -- Refresh and auto close trouble after running tests
+              ---@type neotest.Consumer
+              opts.consumers.trouble = function(client)
+                client.listeners.results = function(adapter_id, results, partial)
+                  if partial then
+                    return
+                  end
+                  local tree = assert(client:get_position(nil, { adapter = adapter_id }))
+
+                  local failed = 0
+                  for pos_id, result in pairs(results) do
+                    if result.status == "failed" and tree:get_key(pos_id) then
+                      failed = failed + 1
+                    end
+                  end
+                  vim.schedule(function()
+                    local trouble = require("trouble")
+                    if trouble.is_open() then
+                      trouble.refresh()
+                      if failed == 0 then
+                        trouble.close()
+                      end
+                    end
+                  end)
+                  return {}
+                end
+              end
+
+              require("neotest").setup(opts)
+            end
+          '';
           keys = [
             {
               __unkeyed-1 = "<leader>t";
