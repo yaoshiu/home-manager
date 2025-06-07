0bffeef6ac788018edf695f23e3a53dfa21d2d5a
Fay Ash <yaoshiu@qq.com>
2025-06-07 23:36:02 +0800

    nixvim: gitsigns configuration

--- editors/nixvim/editor/gitsigns/default.nix
+++ editors/nixvim/editor/gitsigns/default.nix
@@ -1,9 +1,67 @@
 { ... }:
 {
   programs.nixvim = {
-    plugins.gitsigns = {
-      enable = true;
-      lazyLoad.settings.event = "DeferredUIEnter";
+    plugins = {
+      gitsigns = {
+        enable = true;
+        lazyLoad.settings = {
+          before.__raw = ''
+            function()
+              Snacks.toggle({
+                name = "Git Signs",
+                get = function()
+                  return require("gitsigns.config").config.signcolumn
+                end,
+                set = function(state)
+                  require("gitsigns").toggle_signs(state)
+                end,
+              }):map("<leader>uG")
+            end
+          '';
+          event = "DeferredUIEnter";
+        };
+
+        settings = {
+          signs = {
+            add = {
+              text = "▎";
+            };
+            change = {
+              text = "▎";
+            };
+            delete = {
+              text = "";
+            };
+            topdelete = {
+              text = "";
+            };
+            changedelete = {
+              text = "▎";
+            };
+            untracked = {
+              text = "▎";
+            };
+          };
+
+          signs_staged = {
+            add = {
+              text = "▎";
+            };
+            change = {
+              text = "▎";
+            };
+            delete = {
+              text = "";
+            };
+            topdelete = {
+              text = "";
+            };
+            changedelete = {
+              text = "▎";
+            };
+          };
+        };
+      };
     };
   };
 }
