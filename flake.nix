a737c9b4835bbb0ab02ee0030c41427bbe321157
Fay Ash <yaoshiu@qq.com>
2025-06-27 12:36:01 +0800

    nix: update flake

--- flake.nix
+++ flake.nix
@@ -2,6 +2,8 @@
   description = "Home Manager configuration of yaoshiu";
 
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
