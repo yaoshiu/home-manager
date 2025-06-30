2fdf3fd3adba8ad548804c126a16c99a500d6820
Fay Ash <yaoshiu@qq.com>
2025-07-01 00:31:23 +0800

    font: add more fonts

--- fonts/default.nix
+++ fonts/default.nix
@@ -2,10 +2,23 @@
 {
   home.packages = with pkgs; [
     noto-fonts
+    noto-fonts-emoji
+    noto-fonts-extra
+
+    liberation_ttf
+    dejavu_fonts
+    roboto
+
+    source-han-sans-simplified-chinese
+    source-han-serif-simplified-chinese
+    source-han-sans-traditional-chinese
+    source-han-serif-traditional-chinese
     noto-fonts-cjk-sans
     noto-fonts-cjk-serif
-    source-han-serif-simplified-chinese
+    wqy_microhei
     wqy_zenhei
+    arphic-ukai
+    arphic-uming
 
     maple-mono.NF
   ];
